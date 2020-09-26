353c1e12a927e258539ba271c531a3d53796c054
DawidMilerski <dawidmilerski@gmail.com>
2020-09-26 21:13:05 +0200

    Dodanie alertu w script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn1 = document.querySelector('#btn1');
 const btn2 = document.querySelector('#btn2');
+const alert1 = document.querySelector('#alert');
 
 function funkcja(){
     console.log('Twoja stara');
@@ -7,8 +8,12 @@ function funkcja(){
 
 function funkcja2(){
     console.log('Twój stary');
+    $('#alert').toast('show');
 }
 
 
 btn1.addEventListener('click', funkcja);
 btn2.addEventListener('click', funkcja2);
+
+
+
